6b164113a924938ddd9d1948a68fe6baf5b85a09
Allyson Guilherme <allysondev.tech@gmail.com>
2025-08-16 00:51:40 -0300

    feat: add unit test for create product dto

---
+++ UnitTests/CreateProductDtoTests.cs
@@ -0,0 +1,21 @@
+using Application.DTOs;
+
+namespace UnitTests;
+
+public class CreateProductDtoTests
+{
+    [Fact]
+    public void CreateProductDtoShouldAddNotifications()
+    {
+        var createProductDto = new CreateProductDto()
+        {
+            Name = "",
+            Stock = 0,
+            Price = 0,
+        };
+        createProductDto.Validate();
+        
+        Assert.False(createProductDto.IsValid);
+        Assert.True(createProductDto.Notifications.Count == 3);
+    }
+}
