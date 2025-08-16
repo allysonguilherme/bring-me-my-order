80851e1429b3c5dfd010e0373dc86c18c391b47b
Allyson Guilherme <allysondev.tech@gmail.com>
2025-08-16 02:41:44 -0300

    feat: add the dto for product quantity update

---
+++ UnitTests/UpdateProductStockDtoTest.cs
@@ -0,0 +1,19 @@
+using Application.DTOs;
+
+namespace UnitTests;
+
+public class UpdateProductStockDtoTest
+{
+    [Fact]
+    public void CreateProductDtoShouldAddNotifications()
+    {
+        var dto = new UpdateProductStockDto()
+        {
+            Quantity = 0,
+        };
+        dto.Validate();
+        
+        Assert.False(dto.IsValid);
+        Assert.True(dto.Notifications.Count == 1);
+    }
+}
