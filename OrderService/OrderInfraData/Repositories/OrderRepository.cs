a535e5e6400d3263087c9befa2fccf867f26b43e
Allyson Guilherme Gomes <allysondev.tech@gmail.com>
2025-08-16 15:36:54 -0300

    feat: implement the IOrderRepository interface

--- OrderService/OrderInfraData/Repositories/OrderRepository.cs
+++ OrderService/OrderInfraData/Repositories/OrderRepository.cs
@@ -6,32 +6,10 @@ namespace OrderInfraData.Repositories;
 
 public class OrderRepository (ApplicationDbContext dbContext)  : IOrderRepository
 {
-    public async Task<List<Order>> GetAllAsync()
-    {
-        try
-        {
-            return await dbContext.Orders
-                .Include(o => o.OrderProducts)
-                .ToListAsync();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
-    }
-
     public async Task<Order> GetByIdAsync(int id)
     {
         throw new NotImplementedException();
     }
-
-    public async Task<int> CreateAsync(Order order)
-    {
-        await dbContext.Orders.AddAsync(order);
-        return await dbContext.SaveChangesAsync();
-    }
-
     public async Task<int> DeleteAsync(Order order)
     {
         throw new NotImplementedException();
