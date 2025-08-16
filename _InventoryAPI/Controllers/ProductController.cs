fe26045a1323a4fc946215a463dcf18824280786
Allyson Guilherme <allysondev.tech@gmail.com>
2025-08-16 02:04:01 -0300

    feat: add the route to get product by id

--- _InventoryAPI/Controllers/ProductController.cs
+++ _InventoryAPI/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
+using Application.DTOs;
 using Application.Services.Interfaces;
+using Business.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _InventoryAPI.Controllers;
@@ -17,5 +19,35 @@ public class ProductController(IProductFacade productFacade) : ControllerBase
         }
         
         return NoContent();
+    }
+
+    [HttpGet("{id}", Name = "Get")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var product = await productFacade.GetProduct(id);
+
+        if (product is null)
+        {
+            return NotFound();
+        }
+        return Ok(product);
+    }
+
+    [HttpPost(Name = "Create")]
+    public async Task<IActionResult> Create([FromBody] CreateProductDto createProductDto)
+    {
+        createProductDto.Validate();
+        if (!createProductDto.IsValid)
+        {
+            return BadRequest(createProductDto.Notifications);
+        }
+
+        var createProductResult = await productFacade.CreateProduct(new Product(
+            createProductDto.Name,
+            createProductDto.Stock,
+            createProductDto.Price,
+            createProductDto.Description));
+         
+        return createProductResult != null ? Ok("Produto criado com sucesso!") : NoContent();
     }
 }
