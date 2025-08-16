ecb6c6db0d77cc29b1562df865738c4bc3a4bd7b
Allyson Guilherme <allysondev.tech@gmail.com>
2025-08-16 02:03:47 -0300

    feat: add the facade function to get product by id

--- Application/Services/Interfaces/IProductFacade.cs
+++ Application/Services/Interfaces/IProductFacade.cs
@@ -5,4 +5,6 @@ namespace Application.Services.Interfaces;
 public interface IProductFacade
 {
     Task<List<Product>> GetAllProducts();
+    Task<object?> CreateProduct(Product product);
+    Task<Product?> GetProduct(int id);
 }
--- Application/Services/ProductFacade.cs
+++ Application/Services/ProductFacade.cs
@@ -10,4 +10,14 @@ public class ProductFacade (IProductRepository repository) : IProductFacade
     {
         return await repository.GetAll();
     }
+
+    public async Task<object?> CreateProduct(Product product)
+    {
+        return await repository.Create(product);
+    }
+
+    public async Task<Product?> GetProduct(int id)
+    {
+        return await repository.GetById(id);
+    }
 }
