e54ac77ba77e4ee9e4f10a13514c094709a7968c
Allyson Guilherme <allysondev.tech@gmail.com>
2025-08-17 01:54:59 -0300

    grouped database access related files to a proper folder

---
+++ OrderService/OrderInfraData/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using OrderBusiness.Entities;
+using OrderBusiness.Repositories;
+
+namespace OrderInfraData.Repositories;
+
+public class OrderRepository (ApplicationDbContext dbContext)  : IOrderRepository
+{
+    public async Task<List<Order>> GetAllAsync()
+    {
+        try
+        {
+            return await dbContext.Orders
+                .Include(o => o.OrderProducts)
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public async Task<Order?> GetByIdAsync(int id)
+    {
+        try
+        {
+            return await dbContext.Orders
+                .Include(o => o.OrderProducts)
+                .FirstOrDefaultAsync(o => o.Id == id);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public async Task<int> CreateAsync(Order order)
+    {
+        try
+        {
+            await dbContext.Orders.AddAsync(order);
+            return await dbContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public async Task<int> DeleteAsync(Order order)
+    {
+        try
+        {
+            dbContext.Orders.Remove(order);
+            return await dbContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
