992604d4ceeebb1c937e004b26442be2ca400878
Allyson Guilherme <allysondev.tech@gmail.com>
2025-08-18 17:47:03 -0300

    feat: update database on startup with migrations and ef core

--- OrderService/OrderInfraIOC/Dependencies.cs
+++ OrderService/OrderInfraIOC/Dependencies.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OrderApplication.Services;
 using OrderApplication.Services.Interfaces;
@@ -16,6 +17,27 @@ public class Dependencies
     public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
     {
         DependencyInjection.ConfigureDatabase(configuration, services);
+
+        using (var scope = services.BuildServiceProvider().CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var retries = 10;
+            while (retries-- > 0)
+            {
+                try
+                {
+                    db.Database.Migrate();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    if(retries == 0)throw;
+                    Thread.Sleep(5000);
+                }
+            }
+        }
 
         services.AddScoped<IOrderRepository, OrderRepository>();
         services.AddScoped<IOrderFacade, OrderFacade>();
